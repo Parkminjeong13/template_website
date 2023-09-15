4523f696e3f5f69a0544a354ea2e8873d5b245f3
박민정 <ming1320@gmail.com>
2023-09-15 17:47:10 +0900

    230915 수업자료

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import store from "./store";
 import { Provider, useSelector } from "react-redux";
 import Member from "./pages/Member";
 import Login from "./pages/Login";
+import Example from "./example/Example";
 
 function App() {
   
@@ -55,7 +56,8 @@ function Inner(){
       <Aside />
       <Nav/>
       <Routes>
-        <Route path="/" element={<Main/>}></Route>
+        {/* <Route path="/" element={<Main/>}></Route> */}
+        <Route path="/" element={<Example/>}></Route>
         <Route path="/member" element={<Member/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
       </Routes>
