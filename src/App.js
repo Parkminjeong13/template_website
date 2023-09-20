94cecd6ee9f591fa8ab69d63cc1df762915e6862
박민정 <ming1320@gmail.com>
2023-09-20 18:02:54 +0900

    230920 수업자료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import GlobalStyle from "./components/GlobalStyle";
 import Main from "./pages/Main";
 import Aside from "./components/Aside";
 import { ThemeProvider } from "styled-components";
 
 import Nav from "./components/Nav";
-import store, { loggedIn } from "./store";
+import store, { logIn, loggedIn } from "./store";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import Member from "./pages/Member";
 import Login from "./pages/Login";
@@ -15,6 +15,15 @@ import { useEffect } from "react";
 import { collection, doc, getDoc, getFirestore } from "firebase/firestore";
 import Modify from "./pages/Modify";
 import Findemail from "./pages/Findemail";
+import Write from "./pages/Write";
+import Service from "./pages/Service";
+import Notice from "./pages/service/Notice";
+import Online from "./pages/service/Online";
+import Qna from "./pages/service/Qna";
+import Gallery from "./pages/service/Gallery";
+import View from "./pages/View";
+import { useState } from "react";
+import Modal from "./components/Modal";
 
 function App() {
   
@@ -59,6 +68,9 @@ function Inner(){
   const dispatch = useDispatch();
   const uid = sessionStorage.getItem("users");
   console.log(uid)
+  if(uid){
+    dispatch(logIn(uid));
+  }
 
   useEffect(()=>{
 
@@ -82,6 +94,8 @@ function Inner(){
     fetchUser();
   },[dispatch, uid])
 
+  const [isModal, setIsModal] = useState(true);
+  const navigate = useNavigate()
   
   return (
     <ThemeProvider theme={DarkMode}>
@@ -98,6 +112,15 @@ function Inner(){
         <Route path="/logout" element={<Logout/>}></Route>
         <Route path="/modify" element={<Modify/>}></Route>
         <Route path="/findemail" element={<Findemail/>}></Route>
+        <Route path="/write/:board" element={<Write/>}></Route>
+        <Route path="/view/:board/:view" element={<View/>}></Route>
+        <Route path="/view/:board" element={isModal && <Modal error="유효하지 않은 경로입니다." onClose={()=>{navigate('/')}}/>} ></Route>
+        <Route path="/service" element={<Service/>}>
+          <Route path="notice" element={<Notice/>}></Route>
+          <Route path="online" element={<Online/>}></Route>
+          <Route path="qna" element={<Qna/>}></Route>
+          <Route path="gallery" element={<Gallery/>}></Route>
+        </Route>
       </Routes>
     </ThemeProvider>
   )
