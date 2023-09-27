28504084a52a71c32ddefa89e02910ef1d544712
박민정 <ming1320@gmail.com>
2023-09-27 12:21:04 +0900

    230927 수업자료

--- src/components/Ckeditor.js
+++ src/components/Ckeditor.js
@@ -9,6 +9,7 @@ import { faList, faPen } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Modal from './Modal';
 import { useEffect } from 'react';
+import {getStorage, ref, uploadBytesResumable, getDownloadURL} from 'firebase/storage'
 
 const ButtonWrap = styled.div`
     display: flex;
@@ -42,6 +43,8 @@ function Ckeditor({title, postData}) {
     const {board, view} = useParams();
     const [writeData, setWriteData] = useState("");
     const [message, setMessage] = useState("")
+    const [editorInstance, setEditorInstance] = useState(null);
+    const [fileUrl, setFileUrl] = useState("");
 
     useEffect(()=>{
         if(postData){
@@ -69,6 +72,11 @@ function Ckeditor({title, postData}) {
                 })
                 alert("게시글이 성공적으로 수정 되었습니다.")
             }else{
+                const fileInput = document.querySelector("#file").files[0];
+                console.log(fileInput)
+                if(fileInput){
+                    uploadToFirebase(fileInput)
+                }
                 await addDoc(collection(getFirestore(), board), {
                     title : title,
                     content : writeData,
@@ -77,6 +85,7 @@ function Ckeditor({title, postData}) {
                     name : memberProfile.data.name,
                     email : memberProfile.data.email,
                     nickname : memberProfile.data.nickname,
+                    file : fileUrl, 
                     timestamp : serverTimestamp()
                 })
                 alert("게시글이 성공적으로 등록 되었습니다.");
@@ -88,6 +97,39 @@ function Ckeditor({title, postData}) {
         }
     }
 
+    const uploadToFirebase = async (file) =>{
+        const storageRef = ref(getStorage(), 'images/' + file.name);
+        const upload = uploadBytesResumable(storageRef, file);
+
+        return new Promise((resolve, reject)=>{
+            upload.on('state_changed',
+            (snapshot) =>{
+
+            },
+            (error) =>{
+                reject(error)
+            },
+            ()=>{
+                getDownloadURL(upload.snapshot.ref).then((result)=>{
+                    resolve(result)
+                    setFileUrl(result);
+                })
+            }
+            )
+        })
+    }
+
+    function UploadAdapter(editor){
+        editor.plugins.get("FileRepository").createUploadAdapter = (loader) =>{
+            return {
+                upload: async () =>{
+                    const file = await loader.file;
+                    const downURL = await uploadToFirebase(file);
+                    return { default : downURL}
+                }
+            }
+        }
+    }
 
   return (
     <>
@@ -98,8 +140,10 @@ function Ckeditor({title, postData}) {
             data={writeData}
             config={{
                     placeholder: "내용을 입력하세요.",
+                    extraPlugins: [UploadAdapter]
                 }}
             onReady={ editor => {
+                setEditorInstance(editor);
                 // You can store the "editor" and use when it is needed.
                 // console.log( 'Editor is ready to use!', editor );
             } }
@@ -115,6 +159,7 @@ function Ckeditor({title, postData}) {
                 // console.log( 'Focus.', editor );
             } }
         />
+        <input type="file" id="file" />
         <ButtonWrap>
             <Button><Link to="/service/notice"><FontAwesomeIcon icon={faList} />목록</Link></Button>
             <Button onClick={dataSubmit}><FontAwesomeIcon icon={faPen} />완료</Button>
